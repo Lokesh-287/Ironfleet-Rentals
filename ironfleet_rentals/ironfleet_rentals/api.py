db2f74bebbad4f33ff91f5edc51705b2865b2211
Lokesh-287 <lokeshkrishna383@gmail.com>
2026-03-27 12:27:06 +0530

    feat(rental-agreement): add dynamic rental agreement items auto-fetch

--- ironfleet_rentals/ironfleet_rentals/api.py
+++ ironfleet_rentals/ironfleet_rentals/api.py
@@ -7,3 +7,17 @@ def get_leaf_nodes_parent(doctype, txt, searchfield, start, page_len, filters):
     if txt:
         parent_nodes = [p for p in parent_nodes if txt.lower() in p.lower()]
     return [(p,p) for p in parent_nodes if p]
+
+@frappe.whitelist()
+def get_daily_rate(equipment_category):
+    eq=equipment_category
+    while True:
+        equipment_data=frappe.db.get_value("Equipment Category",equipment_category,["default_daily_rental_rate","parent_equipment_category"],
+        as_dict=True
+        )
+        if equipment_data.get("default_daily_rental_rate"):
+            return equipment_data["default_daily_rental_rate"]
+        if not equipment_data.get("parent_equipment_category"):
+                frappe.throw(f"{eq} has no default_daily_rental_rate please set default_daily_rental_rate before making Aggrement ")
+        equipment_category = equipment_data["parent_equipment_category"]
+    
--- ironfleet_rentals/ironfleet_rentals/doctype/rental_agreement/rental_agreement.js
+++ ironfleet_rentals/ironfleet_rentals/doctype/rental_agreement/rental_agreement.js
@@ -13,3 +13,39 @@ frappe.ui.form.on("Rental Agreement", {
         })
     }
 });
+
+frappe.ui.form.on("Rental Agreement Items",{
+    equipment_categorys:function(frm,cdt,cdn){
+        let row=locals[cdt][cdn]
+        frappe.call({
+            method :"ironfleet_rentals.ironfleet_rentals.api.get_daily_rate",
+            args:{
+                equipment_category : row.equipment_categorys
+            },
+            callback:function(r){
+                if (r.message){
+                    row.daily_rate=r.message
+                    row.qty=1
+                    row.total=r.message
+                }
+            }
+        })
+    },
+    qty:function(frm,cdt,cdn){
+        let row=locals[cdt][cdn]
+        row.total=row.daily_rate*row.qty
+    }
+})
+
+// frappe.call({
+// 				method: "frappe.automation.doctype.auto_repeat.auto_repeat.generate_message_preview",
+// 				type: "POST",
+// 				args: {
+// 					name: frm.doc.name,
+// 				},
+// 				callback: function (r) {
+// 					if (r.message) {
+// 						frappe.msgprint(r.message.message, r.message.subject);
+// 					}
+// 				},
+// 			}
