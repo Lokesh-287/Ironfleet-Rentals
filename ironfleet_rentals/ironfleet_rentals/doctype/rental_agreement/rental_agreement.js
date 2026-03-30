50bdfc763c4c46321f243fd1005a3fad7dfacba2
Lokesh-287 <lokeshkrishna383@gmail.com>
2026-03-30 15:04:43 +0530

    fix(rental agreement): fix js error

--- ironfleet_rentals/ironfleet_rentals/doctype/rental_agreement/rental_agreement.js
+++ ironfleet_rentals/ironfleet_rentals/doctype/rental_agreement/rental_agreement.js
@@ -2,118 +2,119 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Rental Agreement", {
-	refresh(frm) {
-		frm.trigger("load_security_deposit_percentage");
-		frm.trigger("recalculate_totals");
-	},
-	start_date(frm) {
-		frm.trigger("recalculate_totals");
-	},
-	expected_end_date(frm) {
-		frm.trigger("recalculate_totals");
-	},
-	discount_percentage(frm) {
-		frm.trigger("recalculate_totals");
-	},
-	items_remove(frm) {
-		frm.trigger("recalculate_totals");
-	},
-	setup(frm) {
-		frm.set_query("equipment_categorys", "items", function () {
-			return {
-				query: "ironfleet_rentals.ironfleet_rentals.api.get_leaf_nodes_parent",
-			};
-		});
-	},
-	load_security_deposit_percentage(frm) {
-		if (frm.security_deposit_percentage !== undefined) {
-			return;
-		}
+  refresh(frm) {
+    frm.trigger("load_security_deposit_percentage");
+    frm.trigger("recalculate_totals");
+  },
+  start_date(frm) {
+    frm.trigger("recalculate_totals");
+  },
+  expected_end_date(frm) {
+    frm.trigger("recalculate_totals");
+  },
+  discount_percentage(frm) {
+    frm.trigger("recalculate_totals");
+  },
+  items_remove(frm) {
+    frm.trigger("recalculate_totals");
+  },
+  setup(frm) {
+    frm.set_query("equipment_categorys", "items", function () {
+      return {
+        query: "ironfleet_rentals.ironfleet_rentals.api.get_leaf_nodes_parent",
+      };
+    });
+  },
+  load_security_deposit_percentage(frm) {
+    if (frm.security_deposit_percentage !== undefined) {
+      return;
+    }
 
-		frappe.db.get_single_value("Rental Settings", "security_deposit_percentage").then((value) => {
-			frm.security_deposit_percentage = value;
-			frm.trigger("recalculate_totals");
-		});
-	},
-	recalculate_totals(frm) {
-		let totalDailyRate = 0;
+    frappe.db.get_single_value("Rental Settings", "security_deposit_percentage").then((value) => {
+      frm.security_deposit_percentage = value;
+      frm.trigger("recalculate_totals");
+    });
+  },
+  recalculate_totals(frm) {
+    let totalDailyRate = 0;
 
-		(frm.doc.items || []).forEach((item) => {
-			const qty = item.qty || 1;
-			const dailyRate = item.daily_rate ;
-			const total = dailyRate * qty;
+    (frm.doc.items || []).forEach((item) => {
+      const qty = flt(item.qty) || 1;
+      const dailyRate = flt(item.daily_rate);
+      const total = dailyRate * qty;
 
-			if (item.total !== total) {
-				frappe.model.set_value(item.doctype, item.name, "total", total);
-			}
+      if (item.total !== total) {
+        frappe.model.set_value(item.doctype, item.name, "total", total);
+      }
 
-			totalDailyRate += total;
-		});
+      totalDailyRate += total;
+    });
 
-		const hasStart = Boolean(frm.doc.start_date);
-		const hasEnd = Boolean(frm.doc.expected_end_date);
-		let rentalDays = 0;
+    const hasStart = Boolean(frm.doc.start_date);
+    const hasEnd = Boolean(frm.doc.expected_end_date);
+    let rentalDays = 0;
 
-		if (hasStart && hasEnd) {
-			rentalDays = frappe.datetime.get_day_diff(frm.doc.expected_end_date, frm.doc.start_date) + 1;
-			rentalDays = Math.max(rentalDays, 0);
-		}
+    if (hasStart && hasEnd) {
+      rentalDays = frappe.datetime.get_day_diff(frm.doc.expected_end_date, frm.doc.start_date) + 1;
+      rentalDays = Math.max(rentalDays, 0);
+    }
 
-		const estimatedTotal = totalDailyRate * rentalDays;
-		const discountPercentage = frm.doc.discount_percentage;
-		const discountAmount = estimatedTotal * discountPercentage / 100;
-		const netRentalTotal = estimatedTotal - discountAmount;
-		const securityDeposit = netRentalTotal * frm.security_deposit_percentage / 100;
-		const grandTotal = netRentalTotal + securityDeposit;
+    const estimatedTotal = totalDailyRate * rentalDays;
+    const discountPercentage = flt(frm.doc.discount_percentage);
+    const discountAmount = estimatedTotal * discountPercentage / 100;
+    const netRentalTotal = estimatedTotal - discountAmount;
+    const securityDepositPercentage = flt(frm.security_deposit_percentage);
+    const securityDeposit = netRentalTotal * securityDepositPercentage / 100;
+    const grandTotal = netRentalTotal + securityDeposit;
 
-		frm.set_value("total_daily_rate", totalDailyRate);
-		frm.set_value("estimate_rental_days", rentalDays);
-		frm.set_value("estimated_total", estimatedTotal);
-		frm.set_value("discount_amount", discountAmount);
-		frm.set_value("security_deposit", securityDeposit);
-		frm.set_value("grand_total", grandTotal);
-	},
+    frm.set_value("total_daily_rate", totalDailyRate);
+    frm.set_value("estimate_rental_days", rentalDays);
+    frm.set_value("estimated_total", estimatedTotal);
+    frm.set_value("discount_amount", discountAmount);
+    frm.set_value("security_deposit", securityDeposit);
+    frm.set_value("grand_total", grandTotal);
+  },
 });
 
 frappe.ui.form.on("Rental Agreement Items", {
-	equipment_categorys(frm, cdt, cdn) {
-		const row = locals[cdt][cdn];
+  equipment_categorys(frm, cdt, cdn) {
+    const row = locals[cdt][cdn];
 
-		frappe.call({
-			method: "ironfleet_rentals.ironfleet_rentals.api.get_daily_rate",
-			args: {
-				equipment_category: row.equipment_categorys,
-			},
-			callback: function (r) {
-				if (r.message) {
-					frappe.model.set_value(cdt, cdn, "daily_rate", flt(r.message));
-					frappe.model.set_value(cdt, cdn, "qty", flt(row.qty) || 1);
-					frm.trigger("recalculate_totals");
-				}
-			},
-		});
-	},
-	qty(frm, cdt, cdn) {
-		const row = locals[cdt][cdn];
-		frappe.model.set_value(cdt, cdn, "total", flt(row.daily_rate) * (flt(row.qty) || 1));
-		frm.trigger("recalculate_totals");
-	},
-	daily_rate(frm, cdt, cdn) {
-		const row = locals[cdt][cdn];
-		frappe.model.set_value(cdt, cdn, "total", flt(row.daily_rate) * (flt(row.qty) || 1));
-		frm.trigger("recalculate_totals");
-	},
+    frappe.call({
+      method: "ironfleet_rentals.ironfleet_rentals.api.get_daily_rate",
+      args: {
+        equipment_category: row.equipment_categorys,
+      },
+      callback: function (r) {
+        if (r.message) {
+          frappe.model.set_value(cdt, cdn, "daily_rate", flt(r.message));
+          frappe.model.set_value(cdt, cdn, "qty", flt(row.qty) || 1);
+          frm.trigger("recalculate_totals");
+        }
+      },
+    });
+  },
+  qty(frm, cdt, cdn) {
+    const row = locals[cdt][cdn];
+    frappe.model.set_value(cdt, cdn, "total", flt(row.daily_rate) * (flt(row.qty) || 1));
+    frm.trigger("recalculate_totals");
+  },
+  daily_rate(frm, cdt, cdn) {
+    const row = locals[cdt][cdn];
+    frappe.model.set_value(cdt, cdn, "total", flt(row.daily_rate) * (flt(row.qty) || 1));
+    frm.trigger("recalculate_totals");
+  },
 });
 
 // frappe.call({
-// 				method: "frappe.automation.doctype.auto_repeat.auto_repeat.generate_message_preview",
-// 				type: "POST",
-// 				args: {
-// 					name: frm.doc.name,
-// 				},
-// 				callback: function (r) {
-// 					if (r.message) {
-// 						frappe.msgprint(r.message.message, r.message.subject);
-// 					}
-// 				},
-// 			}
+//         method: "frappe.automation.doctype.auto_repeat.auto_repeat.generate_message_preview",
+//         type: "POST",
+//         args: {
+//           name: frm.doc.name,
+//         },
+//         callback: function (r) {
+//           if (r.message) {
+//             frappe.msgprint(r.message.message, r.message.subject);
+//           }
+//         },
+//       }
